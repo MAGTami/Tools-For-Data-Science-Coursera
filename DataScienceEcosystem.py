98e93bd69bb9fa8cbc1a566803b0ced70752cedb
MAGTami <cryptoguti3@gmail.com>
2023-04-18 13:14:49 +0200

    Add files via upload
    Added my Jupiter Notebook file in Python format

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# - List popular languagues for Data Science
+# - List popular Libraries for Data Science
+# - Make tables in Jupiter Notebook
+# - List popular Development environments for Data Science
+# - Evaluating simple aritmethic expressions in Jupyter Notebook
+
+# Some of the popular languages that Data Scientists use are:
+# 1) Python
+# 2) R
+# 3) Julia
+# 4) JavaScript
+# 5) Java-ML
+# 6) SQL
+# 7) WEKA
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. Tensorflow
+# 2. Pandas
+# 3. NumPy
+# 4. Lattice
+# 5. ggPlot2
+
+# |Data Science Tools|
+# |------------------|
+# |Jupyter|
+# |Jupyter Lab|
+# |RStudio|
+# |Apache Zeppeling|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# This is a simple arithmetic expression to multiply then add integers
+
+# In[4]:
+
+
+(3*4)+5
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+# In[7]:
+
+
+(200/60)
+
+
+# It would be 3:30 hours
+
+# ## Author
+# Miguel Alexander Gutierrez Tami
